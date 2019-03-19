b284666342e581b279b1f85432c96908b7a85422
Guillaume Vetter <guillaume.vetter@epfl.ch>
2019-03-19 17:40:52 +0100

    ntm

--- src/BounceApp.java
+++ src/BounceApp.java
@@ -1,9 +1,10 @@
-import javax.swing.Timer;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.LinkedList;
 import java.util.Random;
 
+import static com.sun.java.accessibility.util.AWTEventMonitor.addKeyListener;
+import static java.awt.event.KeyEvent.*;
 
 
 public class BounceApp {
@@ -40,6 +41,7 @@ public class BounceApp {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+
             Frame.getInstance().repaint();
         }
     }
