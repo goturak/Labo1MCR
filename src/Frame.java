5f9188d4cd3aaf405e7c684c3ae0b24b308590c6
luca de Carvalho <luca.de.carvalho@gmail.com>
2019-03-19 19:45:50 +0100

    added spawns and clear done

--- src/Frame.java
+++ src/Frame.java
@@ -11,12 +11,16 @@ public class Frame extends JFrame implements  Displayer,KeyListener{
     private static Frame instance;
     private Painter painter;
     private int x, y;
+    private FilledShapeFactory ffs;
+    private  StrokeShapeFactory sfs;
 
     private Frame(int x, int y, Painter p) {
         setSize(x, y);
         this.painter = p;
         this.x = x;
         this.y = y;
+        ffs= new FilledShapeFactory();
+        sfs= new StrokeShapeFactory();
         add(p);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
@@ -60,16 +64,29 @@ public class Frame extends JFrame implements  Displayer,KeyListener{
 
     @Override
     public void keyTyped(KeyEvent e) {
-        System.out.println("nique");
+
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        System.out.println("des");
+
+        if (e.getKeyCode() == KeyEvent.VK_B) {
+            for (int i = 0; i < 10; i++) {
+                getPainter().add(ffs.createSquare());
+                getPainter().add(ffs.createCircle());
+            }
+        }else if (e.getKeyCode() == KeyEvent.VK_F) {
+            for (int i = 0; i < 10; i++) {
+                getPainter().add(sfs.createSquare());
+                getPainter().add(sfs.createCircle());
+            }
+        }else if (e.getKeyCode() == KeyEvent.VK_E) {
+           getPainter().clear();
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        System.out.println("mamanss");
+
     }
 }
