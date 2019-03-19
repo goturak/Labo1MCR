5d63675adaa9c5b37d61351d14f3df16f5ba06a6
Guillaume Vetter <guillaume.vetter@epfl.ch>
2019-03-19 19:58:28 +0100

    FINISHED

--- src/BounceApp.java
+++ src/BounceApp.java
@@ -13,26 +13,7 @@ public class BounceApp {
     // Autres attributs
     public BounceApp() {
 
-        Random r = new Random();
-        LinkedList<Bouncable> shapes = new LinkedList<>();
-        AbstractShapeFactory ssf= new StrokeShapeFactory();
-        AbstractShapeFactory fsf= new FilledShapeFactory();
-
-        for (int i = 0; i < 20; i++) {
-            if (r.nextInt(4) == 0) {
-                shapes.add(fsf.createCircle());
-            }
-            if (r.nextInt(4) == 1) {
-                shapes.add(fsf.createSquare());
-            }
-            if (r.nextInt(4) == 2) {
-                shapes.add(ssf.createSquare());
-            }
-            if (r.nextInt(4) == 3) {
-                shapes.add(ssf.createCircle());
-            }
-        }
-        this.bouncers = shapes;
+        this.bouncers = new LinkedList<>();
         Frame.getInstance().getPainter().setList(bouncers);
 
     }
--- src/Frame.java
+++ src/Frame.java
@@ -7,20 +7,20 @@ import java.awt.event.KeyListener;
 import static java.awt.event.KeyEvent.*;
 import static java.awt.event.KeyEvent.VK_Q;
 
-public class Frame extends JFrame implements  Displayer,KeyListener{
+public class Frame extends JFrame implements Displayer, KeyListener {
     private static Frame instance;
     private Painter painter;
     private int x, y;
     private FilledShapeFactory ffs;
-    private  StrokeShapeFactory sfs;
+    private StrokeShapeFactory sfs;
 
     private Frame(int x, int y, Painter p) {
         setSize(x, y);
         this.painter = p;
         this.x = x;
         this.y = y;
-        ffs= new FilledShapeFactory();
-        sfs= new StrokeShapeFactory();
+        ffs = new FilledShapeFactory();
+        sfs = new StrokeShapeFactory();
         add(p);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
@@ -31,7 +31,7 @@ public class Frame extends JFrame implements  Displayer,KeyListener{
 
     public static Frame getInstance() {
         if (instance == null) {
-            instance = new Frame(500,500, new Painter());
+            instance = new Frame(500, 500, new Painter());
         }
         return instance;
     }
@@ -40,24 +40,26 @@ public class Frame extends JFrame implements  Displayer,KeyListener{
         painter.repaint();
     }
 
-    public Painter getPainter(){ return painter; }
+    public Painter getPainter() {
+        return painter;
+    }
 
-    public int getWidth(){
+    public int getWidth() {
         return super.getWidth();
     }
-    public int getHeight(){
+
+    public int getHeight() {
         return super.getHeight();
     }
 
-    public Graphics2D getGraphics(){
+    public Graphics2D getGraphics() {
         return (Graphics2D) super.getGraphics();
     }
 
-    public void setTitle(String s){
+    public void setTitle(String s) {
         instance.setTitle(s);
     }
 
-    @Override
     public void addKeyListener(KeyAdapter ka) {
         addKeyListener(ka);
     }
@@ -69,21 +71,29 @@ public class Frame extends JFrame implements  Displayer,KeyListener{
 
     @Override
     public void keyPressed(KeyEvent e) {
-
-        if (e.getKeyCode() == KeyEvent.VK_B) {
-            for (int i = 0; i < 10; i++) {
-                getPainter().add(ffs.createSquare());
-                getPainter().add(ffs.createCircle());
-            }
-        }else if (e.getKeyCode() == KeyEvent.VK_F) {
-            for (int i = 0; i < 10; i++) {
-                getPainter().add(sfs.createSquare());
-                getPainter().add(sfs.createCircle());
-            }
-        }else if (e.getKeyCode() == KeyEvent.VK_E) {
-           getPainter().clear();
+        int key = e.getKeyCode();
+        switch (key) {
+            case VK_B:
+                for (int i = 0; i < 10; i++) {
+                    getPainter().add(ffs.createSquare());
+                    getPainter().add(ffs.createCircle());
+                }
+                break;
+            case VK_F:
+                for (int i = 0; i < 10; i++) {
+                    getPainter().add(sfs.createSquare());
+                    getPainter().add(sfs.createCircle());
+                }
+                break;
+            case VK_E:
+                getPainter().clear();
+                break;
+            case VK_Q:
+                this.dispose();
+                break;
         }
     }
+
 
     @Override
     public void keyReleased(KeyEvent e) {
